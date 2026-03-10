1650926633f7f821a8cc82201378f5e94c632aa1
fpaniaguajava <fernando.paniagua.java@gmail.com>
2026-03-10 12:43:38 +0100

    Initial commit

---
+++ src/main/java/com/cursospring/IFCD0014_2026_02_HolaMundoSpringWS/Ifcd0014202602HolaMundoSpringWsApplication.java
@@ -0,0 +1,13 @@
+package com.cursospring.IFCD0014_2026_02_HolaMundoSpringWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ifcd0014202602HolaMundoSpringWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ifcd0014202602HolaMundoSpringWsApplication.class, args);
+	}
+
+}
